55f6d9b2c192f608887f968efe777389dcbf774c
Bryan Smith <bryanesmith@gmail.com>
2012-06-09 19:57:25 -0400

    Plays a major scale.

---
+++ major-scale.sh
@@ -0,0 +1,29 @@
+#/usr/bin/sh
+
+# Should only be one arg, the port
+if [ $# -ne 1 ]; then
+  echo 
+  echo "USAGE: chromatic-scale.sh [port]"
+  echo
+  exit 1
+fi
+
+URL="http://127.0.0.1:$1/"
+
+curl $URL?tone=C4\&duration=200 >& /dev/null
+
+# If first note failed, bail with appropriate message.
+if [ $? != 0 ]; then
+  echo
+  echo "Cannot contact server. Are you sure server at $URL is online?"
+  echo
+  exit 1
+fi
+curl $URL?tone=D4\&duration=200 >& /dev/null
+curl $URL?tone=E4\&duration=200 >& /dev/null
+curl $URL?tone=F4\&duration=200 >& /dev/null
+curl $URL?tone=G4\&duration=200 >& /dev/null
+curl $URL?tone=A4\&duration=200 >& /dev/null
+curl $URL?tone=B4\&duration=200 >& /dev/null
+curl $URL?tone=C5\&duration=200 >& /dev/null
+
